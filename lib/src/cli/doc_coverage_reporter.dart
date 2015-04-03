bde513eca4fceabfed215be7e24bf6eafa696bc9
Sam Rawlins <srawlins@google.com>
2015-04-02 18:40:01 -0700

    Oops. Bad singularize duplicate

--- lib/src/cli/doc_coverage_reporter.dart
+++ lib/src/cli/doc_coverage_reporter.dart
@@ -178,14 +178,6 @@ class DocCoverageReporter {
   }
 }
 
-String singularize(String s) {
-  if (s == 'return') {
-    return s;
-  }
-  // Remove trailing character. Presumably an 's'.
-  return s.substring(0, s.length - 1);
-}
-
 class PackageDocSdk {
   final List<Map> classes = new List();
   Map package;
